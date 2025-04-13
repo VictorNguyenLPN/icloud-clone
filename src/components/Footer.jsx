698878041972a09f5fbe661eaef81fd7a356ad6e
loveCiForever <quanghuy71847@gmail.com>
2025-04-13 13:38:57 +0700

    fix footer's overflow screensize

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-between lg:flex-row text-xs w-screen bg-customBG-darkLess text-gray-200 lg:px-44 py-6 mt-auto">
+    <footer className="flex flex-col items-center justify-between lg:flex-row text-xs w-full bg-customBG-darkLess text-gray-200 lg:px-44 py-6 mt-auto">
       <div className="flex">
         <button className="p-2">
           <a href="https://www.apple.com/support/systemstatus/">
@@ -15,7 +15,7 @@ const Footer = () => {
         </button>
         <div className="p-2">|</div>
         <button className="p-2">
-          <a href="https://www.apple.com/legal/internet-services/icloud/">
+          <a href="https://www.apple.com/legal/internet-services/icloud/">git
             Terms & Conditions
           </a>
         </button>
